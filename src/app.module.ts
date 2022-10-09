f0689850779096a050624393c0eb23391bc60e6e
Edmilson Torres <edmilson.torres@gmail.com>
2022-10-08 21:15:46 -0300

    feat: typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,34 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: entities,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
