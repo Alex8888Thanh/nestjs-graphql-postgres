2c2ebf1f3842a70b443c1d01d5f615ded058484f
Edmilson Torres <edmilson.torres@gmail.com>
2022-10-09 16:01:57 -0300

    feat: add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,21 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
-export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+@Entity()
+export class UserEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column({
+    nullable: false,
+    unique: true,
+  })
+  email: string;
 }
